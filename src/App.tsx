20fdfa946d037801c05fc6168301577c8bf687c3
semaxio <kutchms@gmail.com>
2025-05-13 14:25:18 +0300

    4 commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
         <h1>Проект для обучения по Git</h1>
         <p>First commit</p>
         <p>Second commit</p>
+        <p>3 commit</p>
+        <p>4 commit</p>
       </div>
     </>
   )
